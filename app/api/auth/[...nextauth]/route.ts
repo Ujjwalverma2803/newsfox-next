c00fb86f6688aab7c9e71f8962d4175cb5abeb7d
Intern25timewise <ujjwal@timewise.in>
2025-07-11 14:38:38 +0530

    Changes GNews API

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "./authOptions"; // relative import
+import { authOptions } from "./authOptions";
 
 const handler = NextAuth(authOptions);
 
